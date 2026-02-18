44f764b9319f49c9e941348c351450401bcefc7c
Ярослав <Yaroslav@MacBook-Pro-Aroslav.local>
2026-02-18 14:30:57 +1000

    .

--- frontend/app/layout.jsx
+++ frontend/app/layout.jsx
@@ -11,7 +11,7 @@ export default function RootLayout({ children }) {
     <html lang="ru">
       <body>
         <SidebarProvider>
-          
+          {children}
         </SidebarProvider>
       </body>
     </html>
