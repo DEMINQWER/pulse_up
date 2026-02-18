3d8eb6c7ed3b620264404d904750dbdc39cc6928
Ярослав <Yaroslav@MacBook-Pro-Aroslav.local>
2026-02-18 14:20:47 +1000

    .

--- frontend/app/layout.jsx
+++ frontend/app/layout.jsx
@@ -1,7 +1,5 @@
 import "../styles/globals.css";
 import SidebarProvider from "@/components/SidebarProvider";
-import Navbar from "@/components/Navbar";
-import Sidebar from "@/components/Sidebar";
 
 export const metadata = {
   title: "Pulse",
@@ -13,11 +11,7 @@ export default function RootLayout({ children }) {
     <html lang="ru">
       <body>
         <SidebarProvider>
-          <Navbar />
-          <Sidebar />
-          <main className="main-content">
-            {children}
-          </main>
+          
         </SidebarProvider>
       </body>
     </html>
