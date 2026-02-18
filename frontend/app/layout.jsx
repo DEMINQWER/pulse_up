ea61c7d7d1bcece9a7170ea7794a3e42abae45e2
Ярослав <Yaroslav@MacBook-Pro-Aroslav.local>
2026-02-18 14:12:53 +1000

    .

--- frontend/app/layout.jsx
+++ frontend/app/layout.jsx
@@ -1,4 +1,4 @@
-import "./styles/globals.css";
+import "../styles/globals.css";
 import SidebarProvider from "@/components/SidebarProvider";
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
