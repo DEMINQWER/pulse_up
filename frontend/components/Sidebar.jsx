9e709fccc7cdd02a9f6affbe268b52e2e999d37d
Ярослав <Yaroslav@MacBook-Pro-Aroslav.local>
2026-02-20 19:05:02 +1000

    .

--- frontend/components/Sidebar.jsx
+++ frontend/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 "use client"
+import Link from "next/link"
 
 export default function Sidebar({ open, onClose }) {
   return (
@@ -6,15 +7,16 @@ export default function Sidebar({ open, onClose }) {
       {open && <div className="overlay" onClick={onClose}></div>}
 
       <div className={`sidebar mobile ${open ? "open" : ""}`}>
+
         <button onClick={onClose} style={{marginBottom: 20}}>
           ✕
         </button>
 
-        <div>Чаты</div>
-        <div>Друзья</div>
-        <div>Профиль</div>
-        <div>Настройки</div>
-        <div>Админ</div>
+        <Link href="/chats" onClick={onClose}>Чаты</Link>
+        <Link href="/friends" onClick={onClose}>Друзья</Link>
+        <Link href="/profile" onClick={onClose}>Профиль</Link>
+        <Link href="/settings" onClick={onClose}>Настройки</Link>
+
       </div>
     </>
   )
